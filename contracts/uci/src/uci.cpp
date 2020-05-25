4348e8cad5d942d171115187a1a78606db3455a3
craigbranscom <craigbranscom@gmail.com>
2020-05-25 18:44:50 -0400

    bug fixes

--- contracts/uci/src/uci.cpp
+++ contracts/uci/src/uci.cpp
@@ -20,7 +20,7 @@ ACTION uci::init(string contract_name, string contract_version, name initial_adm
 
     //initialize
     time_point_sec now = time_point_sec(current_time_point());
-    vector<name> initial_noms_list = [];
+    vector<name> initial_noms_list;
     config initial_conf = {
         contract_name, //contract_name
         contract_version, //contract_version
@@ -284,7 +284,7 @@ void uci::catch_broadcast(name ballot_name, map<name, asset> final_results, uint
     if (rec == name("telos.decide")) {
 
         //open config singleton, get config
-        config_singleton configs(get_self(), get_self().value);
+        config_table configs(get_self(), get_self().value);
         auto conf = configs.get();
 
         //if ballot is election ballot
@@ -292,12 +292,16 @@ void uci::catch_broadcast(name ballot_name, map<name, asset> final_results, uint
 
             //initialize
             vector<name> new_custodians_list;
+            uint16_t elected_count = 0;
 
             //TODO: sort results
 
             //loop over results and build new custodians list
-            for (uint16_t i = 0; i < conf.max_custodians; i++) {
-                new_custodians_list.push_back(p->first);
+            for (pair p : final_results) {
+                if (elected_count >= conf.max_custodians) {
+                    break;
+                }
+                new_custodians_list.push_back(p.first);
             }
 
             //open custodians table, get custodians
@@ -316,109 +320,7 @@ void uci::catch_broadcast(name ballot_name, map<name, asset> final_results, uint
             proposals_table proposals(get_self(), get_self().value);
             auto prop_itr = proposals.find(ballot_name.value);
 
-            //if proposal found
-            if (prop_itr != proposals.end()) {
-
-                //open telos decide treasury table, get treasury
-                treasuries_table treasuries(name("telos.decide"), name("telos.decide").value);
-                auto& trs = treasuries.get(VOTE_SYM.code().raw(), "treasury not found");
-
-                //open milestones table, get milestone
-                milestones_table milestones(get_self(), by_ballot_itr->proposal_name.value);
-                auto& ms = milestones.get(uint64_t(by_ballot_itr->current_milestone), "milestone not found");
-
-                //validate
-                check(by_ballot_itr->status == name("inprogress"), "proposal must be in progress");
-                check(ms.status == name("voting"), "milestone status must be voting");
-
-                //initialize
-                bool approve = false;
-                bool refund = false;
-                name new_ms_status = name("failed");
-                name new_prop_status = name("inprogress");
-                asset new_remaining = asset(0, TLOS_SYM);
-
-                asset total_votes = final_results["yes"_n] + final_results["no"_n] + final_results["abstain"_n];
-                asset non_abstain_votes = final_results["yes"_n] + final_results["no"_n];
-
-                asset quorum_thresh = trs.supply * conf.quorum_threshold / 100;
-                asset approve_thresh = non_abstain_votes * conf.yes_threshold / 100;
-
-                asset quorum_refund_thresh = trs.supply * conf.quorum_refund_threshold / 100;
-                asset approve_refund_thresh = non_abstain_votes * conf.yes_threshold / 100;
-
-                //determine approval and refund
-                if (total_votes >= quorum_thresh && final_results["yes"_n] >= approve_thresh && non_abstain_votes.amount > 0) {
-                    
-                    approve = true;
-                    refund = true;
-                    new_ms_status = name("passed");
-
-                } else if (total_votes >= quorum_refund_thresh && final_results["yes"_n] >= approve_refund_thresh && !refund) {
-                    
-                    refund = true;
-
-                }
-                
-                //execute if first milestone
-                if (by_ballot_itr->current_milestone == 1) {
-
-                    if (approve) {
-
-                        //validate
-                        check(conf.available_funds >= by_ballot_itr->total_requested, "insufficient available funds");
-
-                        //update config funds
-                        conf.available_funds -= by_ballot_itr->total_requested;
-                        conf.reserved_funds += by_ballot_itr->total_requested;
-                        configs.set(conf, get_self());
-
-                        //update remaining funds
-                        new_remaining = by_ballot_itr->total_requested;
-
-                    } else {
-
-                        //update proposal status
-                        new_prop_status = name("failed");
-
-                    }
-
-                    //refund if passed refund thresh, not already refunded
-                    if (refund && !by_ballot_itr->refunded) {
-
-                        //give refund
-                        add_balance(by_ballot_itr->proposer, by_ballot_itr->fee); //TODO: keep newballot fee amounts?
-
-                    }
-
-                } else {
-
-                    //execute if current and last milestone both failed
-                    if (!approve && get_milestone_status(by_ballot_itr->proposal_name, by_ballot_itr->current_milestone - 1) == "failed"_n) {
-
-                        //update proposal status
-                        new_prop_status = name("failed");
-
-                    }
-
-                    new_remaining = by_ballot_itr->remaining;
-
-                }
-
-                //update proposal
-                proposals.modify(*by_ballot_itr, same_payer, [&](auto& col) {
-                    col.status = new_prop_status;
-                    col.refunded = refund;
-                    col.remaining = new_remaining;
-                });
-
-                //update milestone
-                milestones.modify(ms, same_payer, [&](auto& col) {
-                    col.status = new_ms_status;
-                    col.ballot_results = final_results;
-                });
-
-            }
+            //TODO: review proposal ballot
 
         }
 
